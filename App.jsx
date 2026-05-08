bad71538b8db416f5de7ffee1d052f14f79aec4e
oluwaferanmie806-debug <oluwaferanmie806@gmail.com>
2026-05-08 17:34:01 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,85 @@
+export default function App () {
+  return (
+    <div>
+      <div className="fixed top-10 w-250 h-15 bg-black rounded-4xl left-35 pt-1 flex">
+        <img src="https://item7go.com/assets/img/item7_navbar_logo-DKP1Difx.png"></img>
+        <select className="border-2 border-white bg-black text-white rounded-4xl h-12 w-70 ml-110">
+          <option>
+            <img src="https://img.icons8.com/?size=160&id=A1JUR9NRH7sC&format=png" className="h-5 w-5"></img>
+            <p>Contact Us on Whatsapp</p>
+          </option>
+          <option>
+            <img src="https://img.icons8.com/?size=160&id=A1JUR9NRH7sC&format=png" className="h-5 w-5"></img>
+            
+            <p>Support line 1</p>
+            <p>+2348093475812</p>
+            
+          </option>
+          <option>
+            <img src="https://img.icons8.com/?size=160&id=A1JUR9NRH7sC&format=png" className="h-5 w-5"></img>
+                        <p>Support line 2</p>
+            <p>+2348093475812</p>
+          </option>
+        </select>
+        
+      </div>
+      <div className="bg-black h-1000 text-white">
+        <div className="flex">
+        <p className="text-6xl text-white pt-40 ml-110">Your</p>
+        <img src="https://item7go.com/assets/img/item7_navbar_logo-DKP1Difx.png" className="h-15 w-60 mt-42"></img>
+        </div>
+        <p className="text-6xl ml-103">Ready on Arrival</p>
+        <p className="ml-107 text-xl mt-5">Feel the Item7GO experience without the wait.</p>
+        <p className="ml-118 text-xl">Order through our mobile app today</p>
+        <div className="flex">
+          <button className="flex items-center bg-white rounded-4xl h-15 w-35 text-black pl-4 ml-100 mt-5">
+            <img src="https://img.icons8.com/?size=60&id=95294&format=png" className="h-5 w-5"></img>
+            <p className="ml-2">Apple Store</p>
+          </button>
+          <button className="flex items-center bg-white rounded-4xl h-15 w-35 text-black pl-4 ml-3 mt-5">
+            <img src="https://img.icons8.com/?size=96&id=L1ws9zn2uD01&format=png" className="h-5 w-5"></img>
+            <p className="ml-2">Play Store</p>
+          </button>
+          <button className="flex items-center bg-white rounded-4xl h-15 w-35 text-black pl-6 ml-3 mt-5">
+            <img src="https://item7go.com/assets/img/Item7%20favicon-C3VyJ8_S.png" className="h-5 w-5"></img>
+            <p>Web App</p>
+          </button>
+        </div>
+        
+        <img src="https://item7go.com/assets/img/iphone%20mockup-C2M2vroD.png" className="ml-70 mt-30 w-180"></img>
+        <p className="text-4xl font-bold ml-120 mt-35">How It Works</p>
+        <div className="flex ml-60 mt-25">
+        <img src="https://item7go.com/assets/img/ready-BFU_RXyQ.svg" className="h-50 w-50"></img>
+        <img src="https://item7go.com/assets/img/set-Bo1TLyPk.svg" className="ml-40 h-30 w-30 mt-10"></img>
+        <img src="data:image/svg+xml,%3csvg%20width='79'%20height='48'%20viewBox='0%200%2079%2048'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M30.7516%2022.9779L31.2338%2012.8516C30.3911%2012.7831%2029.1589%2012.6978%2027.5372%2012.5959C24.7482%2012.4115%2022.5758%2013.0177%2021.02%2014.4147C19.4656%2015.7791%2018.6446%2017.4839%2018.5571%2019.5291C18.4626%2021.7365%2018.8752%2023.4941%2019.7949%2024.8018C20.7146%2026.1095%2022.0009%2026.8313%2023.6537%2026.9671C27.4138%2027.2581%2029.7798%2025.9283%2030.7516%2022.9779ZM37.195%209.2041L36.1531%2034.2442C36.0267%2037.1983%2034.7543%2040.0737%2032.3928%2041.0903C30.0312%2042.1068%2026.3848%2042.6945%2021.3313%2042.4783C19.6432%2042.406%2018.5725%2042.1594%2017.0945%2041.7384C15.649%2041.3188%2014.6093%2040.9166%2013.9753%2040.5317L13.0751%2039.9078L13.3064%2034.5028C13.6213%2034.7439%2014.0315%2035.0379%2014.537%2035.3847C15.075%2035.733%2015.6152%2036.2171%2017.1822%2036.837C18.7479%2037.4894%2020.2442%2037.8624%2021.6712%2037.956C24.5914%2038.1135%2026.7406%2037.6689%2028.1188%2036.6221C29.5281%2035.6092%2030.2834%2033.9178%2030.3849%2031.548L30.5182%2028.4316C29.5648%2029.4315%2028.4426%2030.1966%2027.1515%2030.7267C25.8619%2031.2244%2023.9976%2031.4698%2021.5588%2031.463C18.7614%2031.4734%2016.5328%2030.3536%2014.8729%2028.1036C13.213%2025.8536%2012.449%2023.1866%2012.581%2020.1026C12.7491%2016.1746%2014.1267%2013.2416%2016.7138%2011.3034C19.3348%209.33414%2022.7737%208.39182%2027.0305%208.47642C28.1681%208.49258%2029.5795%208.56924%2031.2648%208.70641C32.9839%208.8125%2034.394%208.92162%2035.4949%209.03378L37.195%209.2041Z'%20fill='white'/%3e%3cpath%20d='M60.6846%2021.4295C60.7749%2019.3194%2060.2494%2017.5407%2059.108%2016.0934C57.9991%2014.6475%2056.4545%2013.8822%2054.4743%2013.7974C52.4616%2013.7113%2050.8234%2014.3729%2049.5598%2015.7824C48.2975%2017.1593%2047.6213%2018.9028%2047.531%2021.0129C47.4393%2023.1554%2047.9641%2024.9503%2049.1055%2026.3976C50.2469%2027.8449%2051.8239%2028.6117%2053.8366%2028.6978C55.8168%2028.7825%2057.4218%2028.1357%2058.6516%2026.7574C59.9152%2025.348%2060.5929%2023.572%2060.6846%2021.4295ZM63.7824%2012.8786C66.3229%2015.0688%2067.4795%2018.0615%2067.252%2021.8569C67.0246%2025.6522%2065.5686%2028.5169%2062.884%2030.4509C60.2334%2032.3538%2057.1226%2033.2289%2053.5517%2033.0761C50.0782%2032.9274%2047.1368%2031.7446%2044.7274%2029.5275C42.352%2027.2794%2041.2413%2024.3537%2041.3955%2020.7503C41.5567%2016.9847%2042.916%2014.0996%2045.4734%2012.0951C48.0309%2010.0906%2051.0626%209.16338%2054.5686%209.31342C58.1719%209.46762%2061.2432%2010.656%2063.7824%2012.8786Z'%20fill='white'/%3e%3cpath%20d='M45.1564%2048.0001C65.6079%2035.4982%2070.6441%2024.6275%2069.2268%200.462939C81.874%2030.3251%2079.5778%2041.8037%2045.1564%2048.0001Z'%20fill='%23FF011A'/%3e%3cpath%20d='M33.6794%200C12.0779%2010.3892%205.97748%2020.7004%204.96576%2044.8853C-4.62477%2013.906%20-1.1897%202.71528%2033.6794%200Z'%20fill='%23FF011A'/%3e%3c/svg%3e" className="ml-40 h-30 w-30 mt-10"></img>
+        </div>
+        {/* yuioohgfdsihjkuyt */}
+
+        <div className="flex">
+          <div className="ml-65">
+            <p className="text-xl">Download The App</p>
+            <p className="mt-5">Download and install the app by</p>
+            <p> clicking the button on this page.</p>
+          </div>
+          <div className="ml-30">
+            <p className="text-xl">Make Your Order</p>
+            <p className="mt-5">Pick your favourite item 7 through</p>
+            <p>the app, customize the order to your</p>
+            <p> heart's content, and pick the closest</p>
+            <p> pick-up location to you.</p>
+          </div>
+          <div className="ml-8">
+            <p className="text-xl">Pick It Up</p>
+            <p className="mt-5">When your order is ready, you'll get</p>
+            <p> notified and you can pick it up!</p>
+          </div>
+        </div>
+        {/* sdfghjkkjdsautrewertyuiuytrew */}
+
+
+  <p className="text-4xl ml-110 font-bold mt-100">FAQ and Walkthrough</p>
+  <p className="text-md ml-125">Got Questions? We've Got Answers!</p>
+      </div>
+    </div>
+  )
+}
